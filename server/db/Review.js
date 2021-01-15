cb3a943435a45de22b986b4517895f2b29fdd36e
Norman Liu <normanwuweiliu@gmail.com>
2021-01-14 19:42:17 -0800

    schema and docs finished

--- server/db/Review.js
+++ server/db/Review.js
@@ -1,21 +1,19 @@
 import mongoose from 'mongoose';
 mongoose.Promise = require('bluebird');
 
+/**@module Review */
+
 const { Schema } = mongoose;
 
-/**
- * @constructor Review
- */
 const ReviewSchema = {
     title: { type: String, required: true, },
-
     user: { type: String, required: true, },
     date: { type: Date, required: true, },
     likes: { type: Number, required: true, },
     body: { type: String, required: true, },
     stars: { type: Number, required: true, },
     recommend: { type: Boolean, required: true, },
-    product: { type: Schema.Types.ObjectId, ref: 'Product', required: true },
+    product: { type: Schema.Types.ObjectId, ref: 'Product'},
     metrics: {
         difficulty: Number,
         value: Number,
@@ -25,6 +23,18 @@ const ReviewSchema = {
     }
 };
 
+/** Review document for our db
+ * @class Review
+ * @prop {string} title - review title
+ * @prop {string} user - username
+ * @prop {date} date - review date
+ * @prop {number} likes - how many updoots it got
+ * @prop {string} body - review text
+ * @prop {number} stars - 1-5
+ * @prop {boolean} recommend - whether a user would recommend this product
+ * @prop {{difficulty: number, value: number, quality: number, appearance: number, works: number}} metrics - these range from 1-5 in each category
+ *
+ */
 const Review = mongoose.model('Review', ReviewSchema);
 
 export default Review;
