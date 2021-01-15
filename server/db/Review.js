5af10a378e6c634ea260087bdcdcfce891049456
Norman Liu <normanwuweiliu@gmail.com>
2021-01-14 19:29:34 -0800

    documentation test

--- server/db/Review.js
+++ server/db/Review.js
@@ -1,28 +1,29 @@
 import mongoose from 'mongoose';
 mongoose.Promise = require('bluebird');
 
-const {Schema} = mongoose;
+const { Schema } = mongoose;
 
-const req = (type) => {return { type, required: true } };
+/**
+ * @constructor Review
+ */
+const ReviewSchema = {
+    title: { type: String, required: true, },
 
-/** @class Review */
-const ReviewSchema = new Schema({
-    title: req(String),
-    user: req(String),
-    date: req(Date),
-    likes: req(Number),
-    body: req(String),
-    stars: req(Number),
-    recommend: req(Boolean),
-    product: { type: Schema.Types.ObjectId, ref: 'Product', required: true},
+    user: { type: String, required: true, },
+    date: { type: Date, required: true, },
+    likes: { type: Number, required: true, },
+    body: { type: String, required: true, },
+    stars: { type: Number, required: true, },
+    recommend: { type: Boolean, required: true, },
+    product: { type: Schema.Types.ObjectId, ref: 'Product', required: true },
     metrics: {
-        difficulty: Number,    
-        value: Number,    
-        quality: Number,    
+        difficulty: Number,
+        value: Number,
+        quality: Number,
         appearance: Number,
-        works: Number,    
+        works: Number,
     }
-});
+};
 
 const Review = mongoose.model('Review', ReviewSchema);
 
