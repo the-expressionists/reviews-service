609969375eb37137935a26121f046a55307a06d6
Norman Liu <normanwuweiliu@gmail.com>
2021-01-25 17:45:15 -0800

    updated gitignore

--- dist/server/endpoints/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";Object.defineProperty(exports, "__esModule", { value: true });Object.defineProperty(exports, "reviewsHandler", { enumerable: true, get: function get() {return _reviews["default"];} });
-
-
-var _reviews = _interopRequireDefault(require("./reviews"));function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { "default": obj };}
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uLy4uL3NlcnZlci9lbmRwb2ludHMvaW5kZXguanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7O0FBR0EsNEQiLCJzb3VyY2VzQ29udGVudCI6WyIvKipcbiAqIEBtb2R1bGUgRW5kcG9pbnRzXG4gKi9cbmltcG9ydCByZXZpZXdzSGFuZGxlciBmcm9tICcuL3Jldmlld3MnO1xuXG5leHBvcnQge1xuICAgIHJldmlld3NIYW5kbGVyXG59OyJdfQ==
